525d29cbeed988783aabf04ecc341c78b7a0bd7a
Simone Romano <simoneromano@tutanota.de>
2021-05-30 14:46:47 +0100

    rename settings -> app_settings

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -1,11 +1,11 @@
+pub mod app_settings;
 pub mod hydra;
 pub mod logger;
 pub mod mongo;
 pub mod server;
-pub mod settings;
 
+pub use app_settings::*;
 pub use hydra::*;
 pub use logger::*;
 pub use mongo::*;
 pub use server::*;
-pub use settings::*;
