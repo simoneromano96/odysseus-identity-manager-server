1ff837fd2b7f34b6701de3ad91fbb64ae35f8589
Simone Romano <simoneromano@tutanota.de>
2021-06-28 23:24:29 +0200

    fix route registration

--- src/auth/routes.rs
+++ src/auth/routes.rs
@@ -1,11 +1,12 @@
 use paperclip::actix::web::ServiceConfig;
 
-use super::{get_consent, get_login, get_logout, post_consent, post_login, post_logout, signup};
+use super::{get_consent, get_login, get_logout, post_consent, post_login, post_logout, signup, validate_email};
 
 /// Configures all the auth routes
 pub fn init_routes(cfg: &mut ServiceConfig) {
 	// Local routes
 	cfg.service(signup);
+	cfg.service(validate_email);
 
 	// Oauth routes
 	cfg.service(get_consent);
